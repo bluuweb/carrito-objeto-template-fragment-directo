fa9852b2d44aa77c844b133eb185e8e965c4d6d0
bluuweb <contacto@bluuweb.cl>
2021-10-29 16:22:28 -0300

    formdata prev

--- app.js
+++ app.js
@@ -1,69 +1,16 @@
-// const formularioQuerySelecto = document.querySelector('form')
-// const userName = document.querySelector("input[name='userName']");
-// const userEmail = document.querySelector("input[name='userEmail']");
-
 const formulario = document.getElementById("formulario");
-const userName = document.getElementById("userName");
-const userEmail = document.getElementById("userEmail");
-
-const alertSuccess = document.getElementById("alertSuccess");
-const alertEmail = document.getElementById("alertEmail");
-const alertName = document.getElementById("alertName");
-
-const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-const regUserEmail =
-    /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
-
-const pintarMensajeExito = () => {
-    alertSuccess.classList.remove("d-none");
-    alertSuccess.textContent = "Mensaje enviado con éxito";
-};
-
-const pintarMensajeError = (errores) => {
-    errores.forEach((item) => {
-        item.tipo.classList.remove("d-none");
-        item.tipo.textContent = item.msg;
-    });
-};
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-    alertSuccess.classList.add("d-none");
 
-    const errores = [];
+    const inputs = new FormData(formulario);
 
-    // esto devuelve true si existe solo espacios
-    // console.log(!userName.value.trim());
-
-    if (!regUserName.test(userName.value) || !userName.value.trim()) {
-        userName.classList.add("is-invalid");
-        errores.push({
-            tipo: alertName,
-            msg: "Formato no válido en el campo nombre, solo letras",
-        });
-    } else {
-        userName.classList.remove("is-invalid");
-        userName.classList.add("is-valid");
-        alertName.classList.add("d-none");
+    for (let item of inputs) {
+        console.log(item);
     }
 
-    if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
-        userEmail.classList.add("is-invalid");
-        errores.push({
-            tipo: alertEmail,
-            msg: "Escriba un correo válido",
-        });
-    } else {
-        userEmail.classList.remove("is-invalid");
-        userEmail.classList.add("is-valid");
-        alertEmail.classList.add("d-none");
-    }
+    console.log(inputs.get("userName"));
+    console.log(inputs.get("userEmail"));
 
-    if (errores.length !== 0) {
-        pintarMensajeError(errores);
-        return;
-    }
-
-    console.log("formulario enviado");
-    pintarMensajeExito();
+    console.log("procesando formulario");
 });
