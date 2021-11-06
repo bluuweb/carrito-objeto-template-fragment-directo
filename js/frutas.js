bf700946515c2589b7a12e6098a72e34d7afdf5a
bluuweb <contacto@bluuweb.cl>
2021-11-06 15:40:55 -0300

    fin poo

---
+++ js/frutas.js
@@ -0,0 +1,19 @@
+// const sandia = "🍉";
+
+// export default sandia;
+
+export default function () {
+    console.log("🍌");
+}
+
+const frutilla = () => {
+    console.log("🍓");
+};
+
+class Fruta {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+}
+
+export { frutilla, Fruta };
